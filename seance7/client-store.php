03c6302465e1f4970b17066b31a29096ac90c9b1
Marcos Sanches <58822240+markitosanches@users.noreply.github.com>
2022-10-17 15:05:50 -0400

    Seance 7

---
+++ seance7/client-store.php
@@ -0,0 +1,36 @@
+<?php
+require_once "bd/connex.php";
+print_r($_POST);
+
+/*$nom = $_POST['nom'];
+$adresse = $_POST['adresse'];
+$postal_code = $_POST['postal_code'];
+$courriel = $_POST['courriel'];
+*/
+
+/*
+foreach($_POST as $key=>$value){
+        $$key = $value;
+}
+*/
+
+extract($_POST);
+
+//die();
+
+
+$sql="INSERT INTO client (nom, adresse, postal_code, courriel) VALUES (?, ?, ?, ?)";
+$stmt = $pdo->prepare($sql);
+if($stmt->execute(array($nom, $adresse, $postal_code, $courriel))){
+    
+    $id = $pdo->lastInsertId();
+
+    header('Location: client-show.php?id='.$id);
+}else{
+    print_r($stmt->errorInfo());
+}
+
+
+
+
+client-show.php?id=7
